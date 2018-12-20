f1b8187f4005a63b9358fecc34b67e6ef7c3f753
subbaparitala <subbaparitala@gmail.com>
2018-12-20 13:56:06 -0600

    add method to fill up the space when user selects the column

--- board.rb
+++ board.rb
@@ -2,14 +2,27 @@ require 'matrix'
 
 class Board
 
-  attr_accessor :board, :row, :col, :match, :player1, :player2
+  attr_accessor :board, :row, :col, :match, :player1, :player2, :turn_number, :options
 
-  def initialize(row=6, col=7, matching=4, name1 = 'Player 1', name2 = 'Player 2')
+  def initialize(row=6, col=7, matching=4)
     @row = row 
     @col = col 
     @match = matching
-    @player1  = Player.new(n)
-    @board = Matrix.send build(@row, @col) {@match}.to_a
+    @player1  = Player.new(name1, 'R')
+    @player2 = Player.new(name2, 'Y')
+    @board = Matrix.build(@row, @col) {'.'}.to_a
+  end
+
+  def fill_space column, sign
+    @row.times do |fill|
+      row = @row - 1 - fill 
+
+      if self[column, row] == "."
+        self[column, row] = sign
+        break
+      end
+
+    end
   end
 
 end
